3bef7e6f2ef1463ab679f5498d5321e54ab75439
Administrator <1585>
2018-03-29 08:45:06 +0800

    Merge branch 'master' of https://github.com/wwwwyy1585/Test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/test/service/impl/TestServiceImpl.java
+++ src/main/java/com/test/service/impl/TestServiceImpl.java
@@ -6,6 +6,7 @@ import com.test.model.User;
 import com.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -14,12 +15,14 @@ public class TestServiceImpl implements TestService{
     @Autowired
     private TestDao testDao;
 
+    @Transactional
     @Override
     public List<User> queryUser() {
         System.out.print("hhhhhhh");
         return testDao.queryUser();
     }
 
+    @Transactional
     @Override
     public int addUser(User users) {
         int n=testDao.addUser(users);
