6dc362284aef4b2dbc50c7241a713f1d62996c53
wwwwyy1585 <35556368+wwwwyy1585@users.noreply.github.com>
2018-04-02 14:31:21 +0800

    Update Test2.java

--- src/main/java/com/sxtx/controller/Test2.java
+++ src/main/java/com/sxtx/controller/Test2.java
@@ -1,7 +1,17 @@
 package com.sxtx.controller;
 
+import com.sxtx.ffmpeg.ConvertVedio;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by wang on 2018/3/30.
  */
 public class Test2 {
+  
+  @RequestMapping("test1")
+    @ResponseBody
+    public String TestVideo(){
+        ConvertVedio.convertVedio("C:\\Users\\Administrator\\Desktop\\dialrotation.mp4");
+        return "hahahahah";
+    }
 }
